648b88169f98a583a84ea1e3ab46c6ff70e9b000
cjdaly <cjdaly@locosoft.com>
2016-04-26 23:18:32 -0700

    show2 setup

---
+++ CompuCanvas-scripts/sudo-setup-show2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+####
+# Copyright (c) 2016 Chris J Daly (github user cjdaly)
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   cjdaly - initial API and implementation
+####
+
+apt-get install arduino -y
+pip install ino
+
+git clone https://github.com/cjdaly/Show2-Eboogaloo.git ~/Show2-Eboogaloo
+ant -f ~/Show2-Eboogaloo/Show2-Eboogaloo-SETUP/setup.xml
